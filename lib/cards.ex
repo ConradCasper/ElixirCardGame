47c178fdc8d2353f8522015a4c7e5af85271104d
Conrad <cnrdcasper@gmail.com>
2020-04-23 21:40:20 -0400

    created shuffle method

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,5 +1,10 @@
 defmodule Cards do
+  @spec create_deck :: [<<_::24, _::_*16>>, ...]
   def create_deck do
     ["Ace", "Two", "Three"]
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
   end
 end
